1e4b56dc5ad20c6a7a9ed737e915f0c5679a3900
Jose Soto <jsoto7087@gmail.com>
2022-04-19 17:23:50 -0400

    commit tree 5a0ee0468057c97befcec3cf5d102982e0db6f05

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+function findMatching(drivers, driverName) {
+  const result = drivers.filter(function (name) {
+    return name.toUpperCase() === driverName.toUpperCase();
+  })
+  return result;
+}
+
+function fuzzyMatch(drivers, driverName) {
+  const result = drivers.filter(function (name) {
+    return name.charAt(1) === driverName.charAt(1);
+  })
+  return result;
+}
+
+function matchName(drivers, driverName) {
+  const finder = drivers.filter(obj => obj.name === `${driverName}`)
+  return finder
+}
